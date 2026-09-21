cbc007a28494899676f8dd85371fad0c72adee61
Herick <jgabrielantunes04@gmail.com>
2026-09-20 23:13:14 -0300

    fix: ajusting script initial with frontend

--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -3,6 +3,12 @@ set -eu
 
 : "${PORT:=80}"
 : "${API_UPSTREAM_URL:=${API_UPSTREAM:-http://api:3000}}"
+case "$API_UPSTREAM_URL" in
+	http://*|https://*) ;;
+	*) API_UPSTREAM_URL="https://$API_UPSTREAM_URL" ;;
+esac
+API_UPSTREAM_URL="${API_UPSTREAM_URL%/}"
+export API_UPSTREAM_URL
 envsubst '${API_UPSTREAM_URL} ${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
